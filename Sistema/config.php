876265d4e3492d2bc49434cf06f95d09c03bdda6
Alvaro <39930744+AlvaroV08@users.noreply.github.com>
2023-07-29 20:46:44 -0500

    Delete Sistema directory

--- Sistema/config.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	$ruta_XML = 'C:\xampp\htdocs\inventario\empresa\xml';
-	$ruta_cache = 'C:\xampp\htdocs\inventario\empresa\cache';
-	$ruta_cert = 'C:\xampp\htdocs\inventario\empresa\cert';
-	$ruta_vendor = 'C:\xampp\htdocs\inventario\vendor';
-?>
